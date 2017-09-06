302ef56d43342a38875be738faabbcae4928cad9
pjsinco <psinco@gmail.com>
2017-09-06 16:58:55 -0500

    Language tweak

--- scripts/progress-bars.js
+++ scripts/progress-bars.js
@@ -174,7 +174,7 @@
     const html = `
       <div class="col text-center p-4" style="">
         <div class="dbviz__container" id="${donutElemId}">
-          ${subs ? '<h6 class="dbviz__title title-alt">Specialty Certification</h6><div id="subSelect"></div>': '<h6 class="dbviz__title">' + title + '</h6>'}
+          ${subs ? '<h6 class="dbviz__title title-alt">Specialty</h6><div id="subSelect"></div>': '<h6 class="dbviz__title">' + title + '</h6>'}
           <div class="dbviz">
             <h4 class="dbviz__count">
               <span class="head">0.0</span> <span class="tail">earned</span>
@@ -450,7 +450,7 @@
 
     $(`#primary .dbviz__title`)
       //.text(newScene.title)
-      .text('Specialty Certification')
+      .text('Specialty')
       .removeClass('title-alt')
 
     if (primary.subs.length > 0) {
